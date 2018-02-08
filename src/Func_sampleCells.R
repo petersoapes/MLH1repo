47360e7ab6d52a470ffe31f239a9374695265ac7
April Peterson <petersoapes@gmail.com>
2018-02-08 15:22:12 -0600

    made function for pvales of diff sample sizes

---
+++ src/Func_sampleCells.R
@@ -0,0 +1,24 @@
+# Sample effect function
+#
+# purpose, for MLH1 df create Big data frame of pvales/ for different sample size distributions
+
+# input: df, sample size
+# return: df
+
+#source("src/Func_sampleCells.R")
+
+
+# sample size
+#sample_cells_pvals
+sample_cells <- function(list, DF, ncells, sim_num){
+  outlist = list()
+  for(mouse in 1:length(unique(list$mouse))){
+    #print(list$mouse[mouse])
+    mus_data <- DF[DF$mouse == list$mouse[mouse],]
+    outlist[mouse] <- list(replicate(sim_num, t.test(sample(DF$adj_nMLH1.foci, ncells, replace = FALSE),
+                                        sample(DF$adj_nMLH1.foci, ncells, replace = FALSE) )$p.value ) )
+    #names(outlist[mouse]) <- c(list$mouse[mouse])
+  }
+  names(outlist) <- c(list$mouse)
+  return(outlist)
+}
