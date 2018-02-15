48eae39521dc5e1e8fc62ff61ed9f5e951dd6096
April Peterson <petersoapes@gmail.com>
2018-02-15 15:51:03 -0600

    fixed stupid bug

--- src/Func_sampleCells.R
+++ src/Func_sampleCells.R
@@ -10,13 +10,13 @@
 
 # sample size
 #sample_cells_pvals
-sample_cells <- function(list, DF, ncells, sim_num){
+sample_cells_pvals <- function(list, DF, ncells, sim_num){
   outlist = list()
   for(mouse in 1:length(unique(list$mouse))){
     #print(list$mouse[mouse])
     mus_data <- DF[DF$mouse == list$mouse[mouse],]
-    outlist[mouse] <- list(replicate(sim_num, t.test(sample(DF$adj_nMLH1.foci, ncells, replace = FALSE),
-                                        sample(DF$adj_nMLH1.foci, ncells, replace = FALSE) )$p.value ) )
+    outlist[mouse] <- list(replicate(sim_num, t.test(sample(mus_data$adj_nMLH1.foci, ncells, replace = FALSE),
+                                        sample(mus_data$adj_nMLH1.foci, ncells, replace = FALSE) )$p.value ) )
     #names(outlist[mouse]) <- c(list$mouse[mouse])
   }
   names(outlist) <- c(list$mouse)
